889746a0cad18695872d3fdae7e250d44fbe882c
abhinavjohncooper <81818070+abhinavjohncooper@users.noreply.github.com>
2022-02-01 22:56:51 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,5 @@
+a1 = int(input("Enter the value 1:" ))
+a2 = int(input("Enter the value 2:" ))
+a3 = int(input("Enter the value 3:" ))
+a4 = int(input("Enter the value 4:" ))
+print("Sum = ", a1+a2+a3+a4)
